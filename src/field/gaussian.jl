7f3cd86b3144ebc5ca4a32ff308c24dd340e6227
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-05-05 12:02:22 +0200

    feat: Gaussian covariance
    - constructor
    - functor
    - practical range
    - c2 derivative
    - spectral moment

--- src/field/gaussian.jl
+++ src/field/gaussian.jl
@@ -1,9 +1,41 @@
+"""
+    GaussianCovariance(phi, d)
+
+Gaussian covariance with scale parameter `phi` used for a field of dimension `d`. It is defined as
+
+```math
+c(r) = \\exp(- r^2 / (2\\phi^2))
+```
+"""
 struct GaussianCovariance{D,T<:Real} <: CovarianceSPP{D,T}
     phi::T
 end
 
-function GaussianCovariance{D}(phi::T) where {D,T<:Real}
-    _check_dimension(Val(D))
-    phi > zero(T) || throw(DomainError(phi, "phi must be positive"))
-    return GaussianCovariance{D,T}(phi)
+# Constructor
+function GaussianCovariance(phi::T, d::Integer) where {T<:Real}
+    _check_dimension(Val(d))
+    phi > zero(T) || throw(DomainError(phi, "the scale parameter must be positive"))
+    return GaussianCovariance{d,T}(phi)
+end
+
+# Functor
+function (cov::GaussianCovariance)(r)
+    return exp(-r^2 / (2 * scale(cov)^2))
+end
+
+# Practical range
+function practical_range(cov::GaussianCovariance, val)
+    (0 < val < 1) || throw(DomainError(val, "the value must satisfy 0 < val < 1"))
+    return sqrt(-2 * scale(cov)^2 * log(val))
+end
+
+# c₂ derivative
+function c2_derivative(cov::GaussianCovariance, s, k::Integer)
+    tmp = 2 * scale(cov)^2
+    return (-1 / tmp)^k * exp(-s / tmp)
+end
+
+# Spectral moment
+function spectral_moment(cov::GaussianCovariance, p::Integer)
+    return prod((1:2:(2 * p))) / scale(cov)^(2 * p)
 end
