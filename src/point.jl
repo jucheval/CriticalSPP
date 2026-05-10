02b992efe697467fa9abe47a285199e79547b3b5
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-05-10 11:44:25 +0200

    feat: constructor for CriticalPointProcess

--- src/point.jl
+++ src/point.jl
@@ -25,9 +25,9 @@ const MAX_CRITICAL = MaxCritical()
 const ALL_CRITICAL = AllCritical()
 
 """
-    CriticalPointProcess{C,CT}
+    CriticalPointProcess(cov, type)
 
-A critical spatial point process defined by a covariance model `C` and a type of critical points `CT`.
+A critical spatial point process with latent GRF with covariance `cov` (with known scale parameter φ) and type of critical points `type` (possible choices are `MAX_CRITICAL` and `ALL_CRITICAL`).
 """
 struct CriticalPointProcess{C<:CovarianceSPP,CT<:AbstractCriticalType}
     cov::C
@@ -37,6 +37,22 @@ function CriticalPointProcess(
     cov::C, ::CT=MAX_CRITICAL
 ) where {C<:CovarianceSPP,CT<:AbstractCriticalType}
     return CriticalPointProcess{C,CT}(cov)
+end
+
+"""
+    CriticalPointProcess(init_cov, type, rho)
+
+When the scale parameter φ of the covariance model is not known, it can be inferred from the target intensity `rho` of the critical point process. 
+
+*Remark:*
+The scale parameter of the input covariance `init_cov` is not used, but other necessary informations (e.g. dimension and smoothness parameter) are inferred from `init_cov`.
+"""
+function CriticalPointProcess(
+    init_cov::C, type::CT, rho::Real
+) where {C<:CovarianceSPP,CT<:AbstractCriticalType}
+    phi = scale_from_intensity(CriticalPointProcess(init_cov, type), rho)
+    cov_scaled = C(phi)
+    return CriticalPointProcess(cov_scaled, type)
 end
 
 """
