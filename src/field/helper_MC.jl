4b72274cfa9cccf605b924213b3ed18a12adf967
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-05-22 21:14:56 +0200

    refactor: `covariance_hessians_x0_xr` and `density_vr`
    - Enforce arguments of same type
    - Remove tests in `type_conservation`

--- src/field/helper_MC.jl
+++ src/field/helper_MC.jl
@@ -5,28 +5,29 @@
 Return the covariance matrix of the upper diagonal and diagonal of the Hessians ∇²X(0) and ∇²X(`r`e₁) given that ∇X(0) = ∇X(`r`e₁) = 0, where X is a Gaussian field with covariance `cov`. See Lemma 8 in Azaïs & Delmas (2022).
 
 ### Arguments
-- `cov::CovarianceSPP`: covariance model.
-- `r::Real`: lag along first coordinate direction.
+- `cov::CovarianceSPP{D,T}`: covariance model.
+- `r::T`: lag along first coordinate direction.
 
 ### Returns
-- `Symmetric`: covariance matrix of the concatenated vectorized Hessians.
+- `Symmetric{T}`: covariance matrix of the concatenated vectorized Hessians.
+
+### Notes
+- Type `T` is common to the two arguments which is not restrictive in a regular use case.
 """
-function covariance_hessians_x0_xr(cov::CovarianceSPP{D,T}, r) where {D,T}
-    R = promote_type(T, typeof(r))
+function covariance_hessians_x0_xr(cov::CovarianceSPP{D,T}, r::T) where {D,T}
     # -----
     # Notations from Lemma 8 in Azaïs & Delmas (2022)
-    ρ = R(r)
+    d = D
+    ρ = r
 
-    𝐫′0 = c2_derivative(cov, zero(R), 1)
-    𝐫′′0 = c2_derivative(cov, zero(R), 2)
+    𝐫′0 = c2_derivative(cov, zero(T), 1)
+    𝐫′′0 = c2_derivative(cov, zero(T), 2)
 
     𝐫′ = c2_derivative(cov, ρ^2, 1)
     𝐫′′ = c2_derivative(cov, ρ^2, 2)
     𝐫′′′ = c2_derivative(cov, ρ^2, 3)
     𝐫′′′′ = c2_derivative(cov, ρ^2, 4)
     # -----
-
-    d = dimension(cov)
 
     # Convention: set at least the upper diagonal and diagonal, 
     # then symmetrize with `Symmetric(..., :U)`.
@@ -39,17 +40,17 @@ function covariance_hessians_x0_xr(cov::CovarianceSPP{D,T}, r) where {D,T}
         Γ₃ += ρ^2 * (𝐫′ + 2ρ^2 * 𝐫′′) / (2 * (𝐫′0^2 - (𝐫′ + 2ρ^2 * 𝐫′′)^2)) * M
         upper_diag = [Γ₁ Γ₃; Γ₃ Γ₁]
     else
-        M = zeros(R, d, d)
+        M = zeros(T, d, d)
         M[2:end, 2:end] .= 16 * 𝐫′′^2
         M[1, 2:end] .= 4 * 𝐫′′ * (12 * 𝐫′′ + 8ρ^2 * 𝐫′′′)
         M[2:end, 1] .= 4 * 𝐫′′ * (12 * 𝐫′′ + 8ρ^2 * 𝐫′′′)
         M[1, 1] = (12 * 𝐫′′ + 8ρ^2 * 𝐫′′′)^2
 
-        Γ₁ = 4 * 𝐫′′0 * ones(R, d, d)
+        Γ₁ = 4 * 𝐫′′0 * ones(T, d, d)
         Γ₁[diagind(Γ₁)] .= 12 * 𝐫′′0
         Γ₁ += ρ^2 * 𝐫′0 / (2 * (𝐫′0^2 - (𝐫′ + 2ρ^2 * 𝐫′′)^2)) * M
 
-        Γ₃ = 4 * 𝐫′′ * ones(R, d, d)
+        Γ₃ = 4 * 𝐫′′ * ones(T, d, d)
         Γ₃[diagind(Γ₃)] .= 12 * 𝐫′′
         Γ₃[1, 2:end] .+= 8ρ^2 * 𝐫′′′
         Γ₃[2:end, 1] .+= 8ρ^2 * 𝐫′′′
@@ -65,7 +66,7 @@ function covariance_hessians_x0_xr(cov::CovarianceSPP{D,T}, r) where {D,T}
         Γ₄ = diagm([D̃₁; D̃₂])
 
         k = d * (d - 1) ÷ 2
-        upper_diag = zeros(R, 2(d + k), 2(d + k))
+        upper_diag = zeros(T, 2(d + k), 2(d + k))
 
         upper_diag[1:d, 1:d] = Γ₁
         upper_diag[(d + k + 1):(2d + k), (d + k + 1):(2d + k)] = Γ₁
@@ -84,27 +85,25 @@ end
 Return the density at 0 of V(`r`) = (∇X(0), ∇X(`r`*e₁)), where X is a Gaussian field with covariance `cov`. See proof of Lemma 8 (beginning of section B.2.1) in Azaïs & Delmas (2022).
 
 ### Arguments
-- `cov::CovarianceSPP`: covariance model.
-- `r::Real`: lag along first coordinate direction.
+- `cov::CovarianceSPP{D,T}`: covariance model.
+- `r::T`: lag along first coordinate direction.
 
 ### Returns
-- `Real`: Gaussian density value at zero for `V(r)`.
+- `T`: Gaussian density value at zero for `V(r)`.
 
 ### Notes
-- The return type is the promoted type between the covariance parameters and `r`.
+- Type `T` is common to the two arguments which is not restrictive in a regular use case.
 """
-function density_vr(cov::CovarianceSPP{D,T}, r) where {D,T}
-    R = promote_type(T, typeof(r))
+function density_vr(cov::CovarianceSPP{D,T}, r::T) where {D,T}
     # -----
     # Notations from Lemma 8 in Azaïs & Delmas (2022)
-    ρ = R(r)
+    d = D
+    ρ = r
 
-    𝐫′0 = c2_derivative(cov, zero(R), 1)
+    𝐫′0 = c2_derivative(cov, zero(T), 1)
     𝐫′ = c2_derivative(cov, ρ^2, 1)
     𝐫′′ = c2_derivative(cov, ρ^2, 2)
     # -----
-
-    d = dimension(cov)
 
     # Convention: set at least the upper diagonal and diagonal, 
     # then symmetrize with `Symmetric(..., :U)`.
@@ -116,7 +115,7 @@ function density_vr(cov::CovarianceSPP{D,T}, r) where {D,T}
     end
 
     Σ = Symmetric(upper_diag, :U)
-    return pdf(MvNormal(Σ), zeros(R, 2d))
+    return pdf(MvNormal(Σ), zeros(T, 2d))
 end
 
 """
