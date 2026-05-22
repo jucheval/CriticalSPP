c18fe9eed4582173f27f3ec62d7e9f7ac415917b
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-05-22 13:57:53 +0200

    fix: tests

--- test/pcf.jl
+++ test/pcf.jl
@@ -14,13 +14,13 @@
     cpp = CriticalPointProcess(cov, ALL_CRITICAL)
     N01 = reshape([1.69, -0.89, -0.07, 0.66, 0.07, 0.2], 6, 1)
     pcf, stderr = CriticalSPP._pair_correlation_single(cpp, r, N01)
-    @test pcf == 0.21688480435720767
+    @test pcf == 0.20518007023706936
 
     cov = RWMCovariance(phi, d)
     cpp = CriticalPointProcess(cov, MAX_CRITICAL)
-    N01 = reshape([-0.19, 1.29, -1.59, -0.22, -0.29, -1.22], 6, 1)
+    N01 = reshape([-0.05, 0.55, -0.53, -0.27, -0.09, 2.09], 6, 1)
     pcf, stderr = CriticalSPP._pair_correlation_single(cpp, r, N01)
-    @test pcf == 0.0973558013558179
+    @test pcf == 1.792705212503323
 end
 
 @testset "pcf_serial" begin
@@ -45,8 +45,8 @@ end
     output = pair_correlation_function(MersenneTwister(1), cpp, r; parallel=:serial)
     @test output == (
         rs=[1.0, 2.0],
-        pcf=[1.185825438681694, 0.9491433166161567],
-        stderr=[5.18352815817258, 3.1353830626716523],
+        pcf=[1.186940633944181, 0.955240250562449],
+        stderr=[5.193355701163643, 3.4092897463239487],
         n_MC=100000,
     )
 
@@ -56,8 +56,8 @@ end
     output = pair_correlation_function(MersenneTwister(1), cpp, r; parallel=:serial)
     @test output == (
         rs=[1.0, 2.0],
-        pcf=[0.004403745642155812, 0.018468257258804625],
-        stderr=[0.21541086193483894, 0.7090098848892278],
+        pcf=[0.0043908073876960255, 0.018212279321504647],
+        stderr=[0.20744815003457243, 0.7006183367647199],
         n_MC=100000,
     )
 end
@@ -85,7 +85,7 @@ end
         MersenneTwister(1), cpp, r; parallel=:threads, n_MC=50000, show_progress=false
     )
     @test isapprox(output.pcf, [1.2112271927892637, 0.9522771225814033], rtol=1e-2)
-    @test isapprox(output.stderr, [5.50894290323668, 3.1070562772609533], rtol=1e-2)
+    @test isapprox(output.stderr, [5.537312015544417, 3.525118765066465], rtol=1e-2)
 
     cov = RWMCovariance(phi, d)
     cpp = CriticalPointProcess(cov, MAX_CRITICAL)
@@ -93,6 +93,6 @@ end
     output = pair_correlation_function(
         MersenneTwister(1), cpp, r; parallel=:threads, n_MC=50000, show_progress=false
     )
-    @test isapprox(output.pcf, [0.004667458716599647, 0.019432243383040823], rtol=1e-2)
-    @test isapprox(output.stderr, [0.2548776852703169, 0.8368493788235667], rtol=1e-2)
+    @test isapprox(output.pcf, [0.004544825814447406, 0.01913452648289884], rtol=1e-2)
+    @test isapprox(output.stderr, [0.23776984264483733, 0.83360883610212], rtol=1e-2)
 end
