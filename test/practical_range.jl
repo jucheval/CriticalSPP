2eeeeed77acb72aa3c58f9e6c3e74551e5092c23
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-06-01 18:09:02 +0200

    test: add coverage of `ArgumentError`

--- test/practical_range.jl
+++ test/practical_range.jl
@@ -19,8 +19,11 @@ end
 end
 
 @testset "RWM covariance" begin
-    for phi in phirange, nu in nurange, val in 0.01:0.1:0.41, d in 2:4
+    for phi in phirange, nu in nurange, val in 0.01:0.1:0.41, d in 1:4
         cov = RWMCovariance(phi, d)
+        d == 1 && @test_throws ArgumentError(
+            "practical range does not make sense for RWM covariance in dimension 1 because the limsup of the covariance is 1 as r goes to infinity",
+        ) practical_range(cov, val)
         pr = practical_range(cov, val)
         for _ in 1:5
             pr += rand() * 0.1 * pr
