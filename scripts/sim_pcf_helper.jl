78beccfe8c6fcc66af74154989d00b2e82b09211
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-06-01 22:32:51 +0200

    modify lag ranges

--- scripts/sim_pcf_helper.jl
+++ scripts/sim_pcf_helper.jl
@@ -5,19 +5,23 @@ const COVARIANCE = Dict(
     ["Matern(" .* string.(nu) .* ")" .=> (d -> MaternCovariance(nu, d)) for nu in NUs]...,
     "RWM" => d -> RWMCovariance(d),
 )
+const RMAX = Dict(1 => 0.02, 2 => 0.3, 3 => 0.4)
 
 # Take a dictionary of parameters, run the simulation and return a dictionary with the results
 function make_pcf(dict::Dict)
     @unpack rho, type, cov, d, nMC = dict
     cpp = CriticalPointProcess(COVARIANCE[cov](d), TYPE[type], rho)
 
-    fmax = type == "all" ? 1.5 : 2.0
-    fmin = 0.005
-    val = 0.1
+    ### I removed the practical_range-based rmax and rmin
+    # fmax = type == "all" ? 1.5 : 2.0
+    # fmin = 0.005
+    # val = 0.1
     # val = which.cov == "RWM" ? 0.2 : 0.1
-    pr = practical_range(cpp.cov, val)
-    rmax = pr * fmax
-    rmin = pr * fmin
+    # pr = practical_range(cpp.cov, val)
+    # rmax = pr * fmax
+    # rmin = pr * fmin
+    rmax = RMAX[d]
+    rmin = 5e-5
 
     rs = range(rmin, rmax; length=Lr)
     pcf = pair_correlation_function(cpp, rs; n_MC=nMC)
