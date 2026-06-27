61c892e2a324ecbf0dd31748d1eb9c2e7f934349
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-06-27 08:10:39 +0200

    remove `RWMCovariance` from `practical_range` comparison with R

--- test/comparison_with_R/practical_range.jl
+++ test/comparison_with_R/practical_range.jl
@@ -21,13 +21,3 @@ end
         @test isapprox(r_val, jl_val; rtol=1e-2) # higher tolerance due to numerical optimization
     end
 end
-
-@testset "RWM covariance" begin
-    for phi in phirange, d in [2], val in range(0.01, 0.21; step=0.02)
-        r_val = rcopy(
-            R"practical.range(rho=NULL, phi=$phi, which.cov=\"RWM\", d=$d, val=$val)"
-        )
-        jl_val = practical_range(RWMCovariance(phi, d), val)
-        @test isapprox(r_val, jl_val)
-    end
-end
